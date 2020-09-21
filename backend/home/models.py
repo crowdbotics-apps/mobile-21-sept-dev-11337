35cdabe6aad6eae5db70db439d0c22c01ebaad6e
Crowdbotics <team@crowdbotics.com>
2020-09-21 06:25:55 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -10,10 +10,6 @@ class CustomText(models.Model):
     title = models.CharField(
         max_length=150,
     )
-    jhgjhgjhg = models.TextField(
-        null=True,
-        blank=True,
-    )
 
     def __str__(self):
         return self.title
